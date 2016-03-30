541e47b58dcef3ef2cf6ea344776140f2d3a42c6
vova_ok <v.gerasimov@neurobotics.ru>
2016-03-30 21:33:34 +0300

    ObjnetStorage initial code

--- src/objnet/objnetstorage.cpp
+++ src/objnet/objnetstorage.cpp
@@ -3,8 +3,42 @@
 using namespace Objnet;
 
 ObjnetStorage* ObjnetStorage::mSelf = 0L;
+Flash::Sector ObjnetStorage::mSector1 = Flash::Sector10;
+Flash::Sector ObjnetStorage::mSector2 = Flash::Sector11;
 
-ObjnetStorage::ObjnetStorage()
+void ObjnetStorage::setFlashSectors(Flash::Sector sector1, Flash::Sector sector2)
+{
+    mSector1 = sector1;
+    mSector2 = sector2;
+}
+
+extern "C" ObjnetStorage *objnetStorage()
+{
+    if (!ObjnetStorage::mSelf)
+    {
+        new ObjnetStorage(ObjnetStorage::mSector1, ObjnetStorage::mSector2);
+    }
+    return ObjnetStorage::mSelf;
+}
+
+ObjnetStorage::ObjnetStorage(Flash::Sector sector1, Flash::Sector sector2)
 {
     mSelf = this;
+    mSector1 = sector1;
+    mSector2 = sector2;
+    
+    mSectorHeader1 = reinterpret_cast<SectorHeader*>(Flash::getBeginOfSector(mSector1));
+    mSectorHeader2 = reinterpret_cast<SectorHeader*>(Flash::getBeginOfSector(mSector2));
 }
+//---------------------------------------------------------------------------
+
+void save(ObjectInfo &info)
+{
+  
+}
+  
+void load(ObjectInfo &info)
+{
+    
+}
+//---------------------------------------------------------------------------
--- src/objnet/objnetstorage.h
+++ src/objnet/objnetstorage.h
@@ -2,17 +2,84 @@
 #define _OBJNETSTORAGE_H
 
 #include "flash.h"
+#include "objectinfo.h"
 
 namespace Objnet
 {
+  
+class ObjnetStorage;
+  
+extern "C" ObjnetStorage *objnetStorage();
   
 class ObjnetStorage
 {
 private:
     static ObjnetStorage *mSelf;
-    ObjnetStorage();
+    static Flash::Sector mSector1, mSector2;
+    ObjnetStorage(Flash::Sector sector1, Flash::Sector sector2);
+    
+    friend ObjnetStorage *objnetStorage();
+    
+#pragma pack(push,1)
+    typedef enum
+    {
+        Erased      = 0xFF,
+        Transfer    = 0x02,
+        Active      = 0x42
+    } MagicNumber;
+    
+    typedef struct
+    {
+        unsigned char magicNumber;
+        unsigned char version;
+        unsigned short useCount;        
+        unsigned long reserve1;
+        unsigned long reserve2;
+        unsigned long reserve3;
+    } SectorHeader;
+    
+    typedef enum
+    {   
+        Desc        = 0x01,
+        Param       = 0x02,
+        Blank       = 0x03,
+        TypeMask    = 0x03,
+        Corrupted   = 0x40,
+        Valid       = 0x80
+    } Flags;
+    
+    typedef struct
+    {
+        Flags flags;
+        unsigned char size;
+        unsigned short nextBlockOffset; // relative to header start
+    } BlockHeader;
+    
+    typedef struct
+    {
+        unsigned short sid;// StorageID
+        unsigned char type; // type of param as in ObjectInfo
+        unsigned char size; // in bytes as in ObjectInfo
+    } DescHeader;
+    
+    typedef struct
+    {
+        unsigned short sid;
+        unsigned short _dummy;
+    } ParamHeader;
+#pragma pack(pop)
+
+    SectorHeader *mSectorHeader1, *mSectorHeader2;
+    
+protected:
+    friend class ObjectInfo;
+    
+    void save(ObjectInfo &info);
+    void load(ObjectInfo &info);
     
 public:
+    static void setFlashSectors(Flash::Sector sector1, Flash::Sector sector2);
+    
     
 };
 
