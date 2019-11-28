4a58b4f3ea9a7fedbd025f983a5dfcb5b3bee433
amyagkov <a.myagkov@neurobotics.ru>
2019-11-28 09:55:56 +0300

    1

--- src/objnet/objnetInterface.h
+++ src/objnet/objnetInterface.h
@@ -91,7 +91,7 @@ public:
         \param mask маска фильтра, по умолчанию все биты выставлены в 1, т.е. идентификатор должен полностью совпадать.
         \return номер фильтра, который можно использовать для удаления, например.
     */
-    virtual int addFilter(uint32_t id, uint32_t mask=0xFFFFFFFF) = 0;
+    virtual int addFilter(unsigned long id, unsigned long mask=0xFFFFFFFF) = 0;
 
     /*! Удаление фильтра.
         \param [in] number номер фильтра, который будет удалён.
